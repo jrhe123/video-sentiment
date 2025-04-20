e3202801150df7683c9b2985e45af3dfb5eb580a
jrhe123 <keith_he@outlook.com>
2025-04-20 15:11:50 -0400

    dataset class

---
+++ training/meld_dataset.py
@@ -0,0 +1,98 @@
+import os
+import cv2
+import pandas as pd
+import numpy as np
+
+from torch.utils.data import Dataset
+from transformers import AutoTokenizer
+
+class MELDDataset(Dataset):
+    def __init__(self, csv_path, video_dir): 
+        self.data = pd.read_csv(csv_path)
+        self.video_dir = video_dir
+
+        self.tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
+        self.emotion_map = {
+            'anger': 0,
+            'disgust': 1,
+            'fear': 2,
+            'joy': 3,
+            'neutral': 4,
+            'sadness': 5,
+            'surprise': 6,
+        }
+        self.sentiment_map = {
+            'negative': 0,
+            'neutral': 1,
+            'positive': 2,
+        }
+    
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        row = self.data.iloc[idx]
+        video_filename = f"""dia{row['Dialogue_ID']}_utt{row['Utterance_ID']}.mp4"""
+        path = os.path.join(self.video_dir, video_filename)
+
+        video_path = os.path.exists(path)
+        if not video_path:
+            print(f"Video {path} not found")
+            raise FileNotFoundError(f"Video {path} not found")
+        
+        # Tokenize text
+        text_inputs = self.tokenizer(
+            row['Utterance'],
+            padding='max_length',
+            truncation=True,
+            max_length=128,
+            return_tensors='pt',
+        )
+
+        # Video frames
+        video_frames = self._load_video_frames(video_path)
+
+    def _load_video_frames(self, video_path):
+        cap = cv2.VideoCapture(video_path)
+        frames = []
+
+        try:
+            if not cap.isOpened():
+                raise ValueError(f"Error opening video {video_path}")
+            
+            # Check the first frame is validate
+            ret, frame = cap.read()
+            if not ret or frame is None:
+                raise ValueError(f"Error reading video {video_path}")
+            
+            # Reset cap to first frame
+            cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+
+            while (len) < 30 and cap.isOpened():
+                ret, frame = cap.read()
+                if not ret:
+                    break
+
+                frame = cv2.resize(frame, (224, 224))
+                frame = frame / 255.0 # normalized to 0~1
+                frames.append(frame)
+
+        except Exception as e:
+            raise ValueError(f"Error loading video {video_path}: {e}")
+        finally:
+            cap.release()
+
+        if len(frames) == 0:
+            raise ValueError(f"No valid frames found in video {video_path}")
+        
+        # Pad or truncate frames
+        if len(frames) < 30:
+
+
+
+
+if __name__ == '__main__':
+    meld = MELDDataset(
+        csv_path='../dataset/dev/dev_sent_emo.csv',
+        video_dir='../dataset/dev/dev_splits_complete',
+    )
